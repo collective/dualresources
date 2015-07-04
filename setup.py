f3c4b36ebb908257705cdbe9a9fd1d917826f353
Sune Broendum Woeller <sune@woeller.dk>
2015-07-05 01:57:34 +0200

    remove plone version classifier - pypi returns Invalid classifier on registration.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3.6",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
     ],
